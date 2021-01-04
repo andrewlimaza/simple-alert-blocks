cb0b8b219d853098fde2a8ea5b155c4ae66c063a
Andrew Lima <andrew@arctek.co.za>
2021-01-04 18:05:49 +0200

    Removed hide-alert.js from build folder.
    Make life easier, move non react code to a JS folder.

--- build/hide-alert.js
+++
@@ -1,8 +0,0 @@
-//Jquery to hide junk, not working in REACT for some reason
-jQuery(document).ready(function(){
-	jQuery( 'button.close' ).on( 'click', function() { 
-    	jQuery(this).closest('.wp-block-simple-alerts-for-gutenberg-alert-boxes').fadeOut('slow');
-	});
-});
-
-//TODO add a cookie storage here sometime to keep things hidden.
